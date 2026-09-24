2931ed28d2c5ef40a54272ca9d0edd5e13c9bb64
jainanthan2005-eng <jainanthan2005@gmail.com>
2026-09-24 19:24:06 +0530

    Add files via upload

---
+++ jai7.py
@@ -0,0 +1,33 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("enter the first value"))
+enter the first value34
+>>> square=a*a
+>>> print(square)
+1156
+>>> a=int(input("enter the first value:"))
+enter the first value:18
+>>> cube=a*a*a
+>>> print(cube)
+5832
+>>> cube=a**a
+>>> 
+... a=int(input("enter the first value:"))
+... 
+enter the first value:6
+>>> cube=a**a
+>>> 
+>>> print(cube)
+46656
+>>> b=int(input("enter the first value:"))
+enter the first value:6
+>>> c=b**b
+>>> print(c)
+46656
+>>> 
+... d=int(input("enter the first value:"))
+enter the first value:3
+>>> print(d**d)
+27
+>>> print(d***d)
+SyntaxError: invalid syntax
