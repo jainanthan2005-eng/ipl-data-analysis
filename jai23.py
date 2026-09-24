22e25a8b371b30791007afd00da8cbd14f6b4e88
jainanthan2005-eng <jainanthan2005@gmail.com>
2026-09-24 19:30:15 +0530

    Add files via upload

---
+++ jai23.py
@@ -0,0 +1,12 @@
+def balance():
+    return 100
+b=balance()
+if b==0:
+    print("Balance of:",b)
+elif b<=0:
+    print("Balance of:",b," negative deposit amt")
+else:
+    print("Balance of:",b)
+
+    
+          
